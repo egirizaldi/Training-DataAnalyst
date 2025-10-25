e071b917ab66e882e70a41768b91ed9ab6ac62a0
egirizaldi <egirzld26@gmail.com>
2025-10-25 15:42:56 +0700

    add main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import pandas as pd
-df = pd.read_csv("sales_data.csv")
-df.head()
+
+def load_data(filepath):
+    """Membaca dataset dari file CSV."""
+    return pd.read_csv(filepath)
+
+def clean_data(df):
+    """Membersihkan data dari duplikat dan nilai kosong."""
+    df = df.drop_duplicates()
+    df = df.fillna(0)
+    return df
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_sales_by_category(df, category_col, sales_col):
+    """Membuat grafik total penjualan per kategori."""
+    plt.figure(figsize=(8,5))
+    sns.barplot(x=category_col, y=sales_col, data=df, estimator=sum)
+    plt.title('Total Sales per Category')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
